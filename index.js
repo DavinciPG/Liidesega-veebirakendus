a4b21606598001bb1c2f2cbc8978da6e2cef663d
Erkz <Erkiparna7@gmail.com>
2023-09-18 21:25:34 +0300

    Harjutus 3

--- index.js
+++ index.js
@@ -1,59 +1,102 @@
-var calculationCount = {
-    inchesToCm: 0,
-    cmToInches: 0,
-    kmPerHourToMPerSec: 0,
-    mPerSecToKmPerHour: 0
-};
-var InchesToCm = /** @class */ (function () {
-    function InchesToCm() {
-    }
-    InchesToCm.prototype.calculate = function (inches) {
-        return inches * 2.54;
-    };
-    return InchesToCm;
-}());
 var CmToInches = /** @class */ (function () {
     function CmToInches() {
     }
     CmToInches.prototype.calculate = function (cm) {
         return cm / 2.54;
     };
+    CmToInches.prototype.inputUnit = function () {
+        return "cm";
+    };
+    CmToInches.prototype.outputUnit = function () {
+        return "in";
+    };
     return CmToInches;
 }());
-function convert() {
-    var inputValue = parseFloat(document.getElementById("inputValue").value);
-    var converterSelect = document.getElementById("converterSelect");
-    var selectedConverter = converterSelect.value;
-    var result = 0;
-    switch (selectedConverter) {
-        case "inchesToCm":
-            result = new InchesToCm().calculate(inputValue);
-            calculationCount.inchesToCm++;
-            break;
-        case "cmToInches":
-            result = new CmToInches().calculate(inputValue);
-            calculationCount.cmToInches++;
-            break;
-        case "kmPerHourToMPerSec":
-            result = inputValue * (1000 / 3600);
-            calculationCount.kmPerHourToMPerSec++;
-            break;
-        case "mPerSecToKmPerHour":
-            result = inputValue * (3600 / 1000);
-            calculationCount.mPerSecToKmPerHour++;
-            break;
-        default:
-            alert("Invalid converter selection.");
-            break;
+var InchesToCm = /** @class */ (function () {
+    function InchesToCm() {
     }
-    document.getElementById("result").textContent = result.toFixed(2);
-    updateCalculationCount();
-}
-function updateCalculationCount() {
-    var calculationCountElement = document.getElementById("calculationCount");
-    calculationCountElement.textContent = Object.keys(calculationCount).map(function (key) { return "".concat(key, ": ").concat(calculationCount[key]); }).join(', ');
-}
-var convertButton = document.getElementById("convertButton");
-if (convertButton) {
-    convertButton.addEventListener("click", convert);
-}
+    InchesToCm.prototype.calculate = function (inches) {
+        return inches * 2.54;
+    };
+    InchesToCm.prototype.inputUnit = function () {
+        return "in";
+    };
+    InchesToCm.prototype.outputUnit = function () {
+        return "cm";
+    };
+    return InchesToCm;
+}());
+var AXplusB = /** @class */ (function () {
+    function AXplusB(coefficient, intercept, inputUnitType, outputUnitType) {
+        this.coefficient = coefficient;
+        this.intercept = intercept;
+        this.inputUnitType = inputUnitType;
+        this.outputUnitType = outputUnitType;
+    }
+    AXplusB.prototype.calculate = function (x) {
+        return this.coefficient * x + this.intercept;
+    };
+    AXplusB.prototype.inputUnit = function () {
+        return this.inputUnitType;
+    };
+    AXplusB.prototype.outputUnit = function () {
+        return this.outputUnitType;
+    };
+    return AXplusB;
+}());
+var taxiFareCalculator = new AXplusB(2.5, 5, "km", "EUR");
+var distanceKm = 10;
+var fare = taxiFareCalculator.calculate(distanceKm);
+console.log("Taxi fare for ".concat(distanceKm, " km: ").concat(fare, " EUR"));
+var celsiusToFahrenheit = new AXplusB(9 / 5, 32, "°C", "°F");
+var celsiusDegrees = 10;
+var fahrenheitDegrees = celsiusToFahrenheit.calculate(celsiusDegrees);
+console.log("".concat(celsiusDegrees, " \u00B0C is equivalent to ").concat(fahrenheitDegrees, " \u00B0F"));
+var fahrenheitToCelsius = new AXplusB(5 / 9, -32 * (5 / 9), "°F", "°C");
+var fahrenheitDegrees2 = 50;
+var celsiusDegrees2 = fahrenheitToCelsius.calculate(fahrenheitDegrees2);
+console.log("".concat(fahrenheitDegrees2, " \u00B0F is equivalent to ").concat(celsiusDegrees2, " \u00B0C"));
+var OhmsLaw = /** @class */ (function () {
+    function OhmsLaw(resistance) {
+        this.resistance = resistance;
+    }
+    OhmsLaw.prototype.calculate = function (voltage) {
+        return voltage / this.resistance;
+    };
+    OhmsLaw.prototype.inputUnit = function () {
+        return "V";
+    };
+    OhmsLaw.prototype.outputUnit = function () {
+        return "A";
+    };
+    OhmsLaw.prototype.setResistance = function (resistance) {
+        this.resistance = resistance;
+    };
+    return OhmsLaw;
+}());
+var ohmsLawCalculator = new OhmsLaw(10);
+var voltage = 12; // Pinge 12 volti
+var current = ohmsLawCalculator.calculate(voltage);
+console.log("Current: ".concat(current, " A"));
+var updatedResistance = 20;
+ohmsLawCalculator.setResistance(updatedResistance);
+var current2 = ohmsLawCalculator.calculate(voltage);
+console.log("Current with updated resistance: ".concat(current2, " A"));
+var PowerCalculator = /** @class */ (function () {
+    function PowerCalculator(resistance) {
+        this.resistance = resistance;
+    }
+    PowerCalculator.prototype.calculate = function (voltage) {
+        return (voltage * voltage) / this.resistance;
+    };
+    PowerCalculator.prototype.inputUnit = function () {
+        return "V";
+    };
+    PowerCalculator.prototype.outputUnit = function () {
+        return "W";
+    };
+    return PowerCalculator;
+}());
+var powerCalculator = new PowerCalculator(10);
+var power = powerCalculator.calculate(voltage);
+console.log("Power: ".concat(power, " W"));
--- index.ts
+++ index.ts
@@ -1,60 +1,105 @@
-let calculationCount: { [key: string]: number } = {
-    inchesToCm: 0,
-    cmToInches: 0,
-    kmPerHourToMPerSec: 0,
-    mPerSecToKmPerHour: 0
-};
-
-class InchesToCm {
-    calculate(inches: number): number {
-        return inches * 2.54;
-    }
+interface CalculatingFunction {
+    calculate(x: number): number;
+    inputUnit(): string;
+    outputUnit(): string;
 }
 
-class CmToInches {
+class CmToInches implements CalculatingFunction {
     calculate(cm: number): number {
         return cm / 2.54;
     }
-}
-
-function convert() {
-    const inputValue = parseFloat((document.getElementById("inputValue") as HTMLInputElement).value);
-    const converterSelect = document.getElementById("converterSelect") as HTMLSelectElement;
-    const selectedConverter = converterSelect.value;
-    let result = 0;
-
-    switch (selectedConverter) {
-        case "inchesToCm":
-            result = new InchesToCm().calculate(inputValue);
-            calculationCount.inchesToCm++;
-            break;
-        case "cmToInches":
-            result = new CmToInches().calculate(inputValue);
-            calculationCount.cmToInches++;
-            break;
-        case "kmPerHourToMPerSec":
-            result = inputValue * (1000 / 3600);
-            calculationCount.kmPerHourToMPerSec++;
-            break;
-        case "mPerSecToKmPerHour":
-            result = inputValue * (3600 / 1000);
-            calculationCount.mPerSecToKmPerHour++;
-            break;
-        default:
-            alert("Invalid converter selection.");
-            break;
+    inputUnit(): string {
+        return "cm";
     }
-
-    document.getElementById("result")!.textContent = result.toFixed(2);
-    updateCalculationCount();
+    outputUnit(): string {
+        return "in";
+    }
 }
 
-function updateCalculationCount() {
-    const calculationCountElement = document.getElementById("calculationCount");
-    calculationCountElement!.textContent = Object.keys(calculationCount).map(key => `${key}: ${calculationCount[key]}`).join(', ');
+class InchesToCm implements CalculatingFunction {
+    calculate(inches: number): number {
+        return inches * 2.54;
+    }
+    inputUnit(): string {
+        return "in";
+    }
+    outputUnit(): string {
+        return "cm";
+    }
 }
 
-const convertButton = document.getElementById("convertButton");
-if (convertButton) {
-    convertButton.addEventListener("click", convert);
+class AXplusB implements CalculatingFunction {
+    constructor(
+        protected coefficient: number,
+        protected intercept: number,
+        protected inputUnitType: string,
+        protected outputUnitType: string
+    ) {}
+    calculate(x: number): number {
+        return this.coefficient * x + this.intercept;
+    }
+    inputUnit(): string {
+        return this.inputUnitType;
+    }
+    outputUnit(): string {
+        return this.outputUnitType;
+    }
 }
+
+const taxiFareCalculator = new AXplusB(2.5, 5, "km", "EUR");
+const distanceKm = 10;
+const fare = taxiFareCalculator.calculate(distanceKm);
+console.log(`Taxi fare for ${distanceKm} km: ${fare} EUR`);
+
+const celsiusToFahrenheit = new AXplusB(9 / 5, 32, "°C", "°F");
+const celsiusDegrees = 10;
+const fahrenheitDegrees = celsiusToFahrenheit.calculate(celsiusDegrees);
+console.log(`${celsiusDegrees} °C is equivalent to ${fahrenheitDegrees} °F`);
+
+const fahrenheitToCelsius = new AXplusB(5 / 9, -32 * (5 / 9), "°F", "°C");
+const fahrenheitDegrees2 = 50;
+const celsiusDegrees2 = fahrenheitToCelsius.calculate(fahrenheitDegrees2);
+console.log(`${fahrenheitDegrees2} °F is equivalent to ${celsiusDegrees2} °C`);
+
+class OhmsLaw implements CalculatingFunction {
+    constructor(protected resistance: number) {}
+    calculate(voltage: number): number {
+        return voltage / this.resistance;
+    }
+    inputUnit(): string {
+        return "V";
+    }
+    outputUnit(): string {
+        return "A";
+    }
+    setResistance(resistance: number): void {
+        this.resistance = resistance;
+    }
+}
+
+const ohmsLawCalculator = new OhmsLaw(10);
+const voltage = 12; // Pinge 12 volti
+const current = ohmsLawCalculator.calculate(voltage);
+console.log(`Current: ${current} A`);
+
+const updatedResistance = 20;
+ohmsLawCalculator.setResistance(updatedResistance);
+const current2 = ohmsLawCalculator.calculate(voltage);
+console.log(`Current with updated resistance: ${current2} A`);
+
+class PowerCalculator implements CalculatingFunction {
+    constructor(protected resistance: number) {}
+    calculate(voltage: number): number {
+        return (voltage * voltage) / this.resistance;
+    }
+    inputUnit(): string {
+        return "V";
+    }
+    outputUnit(): string {
+        return "W";
+    }
+}
+
+const powerCalculator = new PowerCalculator(10);
+const power = powerCalculator.calculate(voltage);
+console.log(`Power: ${power} W`);
